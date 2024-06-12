d1d8064f56d59e62fe4cf4f917bacf56e0af87a2
Tang Yu Xuan <67047462+doodledaron@users.noreply.github.com>
2024-06-12 13:46:15 +0800

    minor change

--- src/services.js
+++ src/services.js
@@ -4,7 +4,7 @@ import { useAuthStore } from './store/authStore.js';
 
 // Create an Axios instance
 const apiClient = axios.create({
-    baseURL: 'https://20.2.198.214:8000/admin/', 
+    baseURL: 'http://127.0.0.1:8000/admin/', 
     withCredentials: false, 
     headers: {
         'Accept': 'application/json',
